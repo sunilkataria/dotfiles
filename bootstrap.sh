305c72144745d4fc9806a48cf1e8538bcc418a01
Sunil Kataria <sunilkataria@outlook.com>
2018-08-25 17:04:26 +0530

    clone ansible-repo as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+cd $HOME
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-git clone https://github.com/powerline/fonts.git --depth=1
+git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
 cd fonts
 ./install.sh
 cd ..
 #rm -rf fonts
 
-git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
-cd awesome-terminal-fonts
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git ~/awesome-terminal-fonts
+cd ~/awesome-terminal-fonts
 ./build.sh
 ./install.sh
-cd ..
+cd -
 #rm -rf awesome-terminal-fonts
 
 
-cd $HOME
-mkdir -p .bash/themes/agnoster-bash
+mkdir -p $HOME/.bash/themes/agnoster-bash
 git clone https://github.com/speedenator/agnoster-bash.git .bash/themes/agnoster-bash
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
 
-curl -fsSLO https://raw.githubusercontent.com/Vaelatern/init-dotfiles/master/init_dotfiles.sh
-chmod +x ./init_dotfiles.sh
-./init_dotfiles.sh
+#curl -fsSLO https://raw.githubusercontent.com/Vaelatern/init-dotfiles/master/init_dotfiles.sh ~/
+#chmod +x ~/init_dotfiles.sh
+#~/init_dotfiles.sh
+
+
+mkdir -p git-repos
+git clone https://github.com/ansible/ansible.git ~/git-repos/ansible-repo
+
